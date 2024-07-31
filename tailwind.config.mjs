646a8998a200cb6f439fc9565fe044a7cfda55ea
EricMartinezLabrin <98716898+EricMartinezLabrin@users.noreply.github.com>
2024-07-30 23:01:04 -0600

    feat: Add dark mode support and update image paths

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,7 @@
 import defaultTheme from "tailwindcss/defaultTheme";
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkmode: "class",
   theme: {
     extend: {
       fontFamily: {
